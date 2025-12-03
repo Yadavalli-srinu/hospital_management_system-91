af24017acc0fde1e341ea70c32a373ee7d9bf1a0
Yadavalli srinu <yadavallis9@gmail.com>
2025-12-03 19:22:31 +0530

    your message

--- hospital_project/settings.py
+++ hospital_project/settings.py
@@ -88,10 +88,10 @@ if os.environ.get("RENDER"):
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.mysql",
-            "NAME": os.environ.get("MYSQLDATABASE", ""),
-            "USER": os.environ.get("MYSQLUSER", ""),
-            "PASSWORD": os.environ.get("MYSQLPASSWORD", ""),
-            "HOST": os.environ.get("MYSQLHOST", ""),
+            "NAME": os.environ.get("MYSQLDATABASE"),
+            "USER": os.environ.get("MYSQLUSER"),
+            "PASSWORD": os.environ.get("MYSQLPASSWORD"),
+            "HOST": os.environ.get("MYSQLHOST"),
             "PORT": os.environ.get("MYSQLPORT", "3306"),
         }
     }
